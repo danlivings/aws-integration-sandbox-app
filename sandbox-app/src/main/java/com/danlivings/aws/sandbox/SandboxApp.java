904e8bd027ce1c937679fada3d16e6bcd0a774b5
Dan Livings <dan.livings@live.com>
2021-03-11 22:43:15 +0000

    Add example web controller

---
+++ sandbox-app/src/main/java/com/danlivings/aws/sandbox/SandboxApp.java
@@ -0,0 +1,13 @@
+package com.danlivings.aws.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxApp.class, args);
+    }
+
+}
---
+++ sandbox-app/src/test/java/com/danlivings/aws/sandbox/SandboxAppTests.java
@@ -0,0 +1,13 @@
+package com.danlivings.aws.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
